e74db2baa04934ff2f773f86ef7d852548aab9e1
Ryan Phillips <ryanmphill@gmail.com>
2023-09-26 16:53:52 -0500

    Removed useEffect and unnecessary state variables used for calculating map bounds and replaced with function wrapped in a useMemo for better readability and performance

--- src/components/studies/studyDetails/StudyMap.jsx
+++ src/components/studies/studyDetails/StudyMap.jsx
@@ -6,7 +6,7 @@ import Map, {
     FullscreenControl,
     GeolocateControl, 
     } from 'react-map-gl';
-  import { useState, useEffect, useMemo} from 'react'
+  import { useState, useMemo} from 'react'
   import 'mapbox-gl/dist/mapbox-gl.css';
   import Pin from './Pin';
   import './studyMap.css';
@@ -17,39 +17,34 @@ import Map, {
   
     const [popupDisplay, setPopupDisplay] = useState(null)
     const [currentGeolocation, setCurrentGeolocation] = useState(null)
-    // State needed to set the initial view of the map
-    const [initialMapBounds, setInitialMapBounds] = useState(null)
-    const [startingViewLoaded, setViewLoaded] = useState(false)
 
-    // MAKE ALL MARKERS VISIBLE ON INITIAL RENDERING OF MAP ///////////////////////////////////////////////////////////
-    /* This useEffect creates an instance of `LngLatBounds` from the MapboxGl library that the Map can use to 
+    // MAKE ALL MARKERS VISIBLE ON INITIAL RENDERING OF MAP
+    /* This function creates an instance of `LngLatBounds` from the MapboxGl library that the Map can use to 
        set the initial view and allow all of the observation markers to be visible. The LngLatBounds object 
        characterizes an imaginary bounding box with two properties: a coordinate for the northeast corner, and 
        a coordinate for the southwest corner. First, an object with null values is initialized. Next, the .extend()
        method incrementally expands the bounding box to include each coordinates for the markers in the `observations`
        array. As this happens, the NE and SW properties are automatically calculated and normalized to account for
        point near the edge of the map or across the antimeridian.  */
-    useEffect(
-        () => {
-            if (observations.length >= 1 && !startingViewLoaded && initialMapBounds === null) {
-                let mapBounds = new mapboxgl.LngLatBounds()
-                observations.forEach((obs) => {
-                  mapBounds.extend([obs.longitude, obs.latitude])
-                })
-                console.log(mapBounds)
-                setInitialMapBounds(mapBounds)
-                setViewLoaded(true)
-            // If there are no observations yet, set startingViewLoaded to true and leave initialMapBounds null 
-            } else if (observations.length < 1 && !startingViewLoaded && initialMapBounds === null) {
-                /* Set startingViewLoaded to true to allow rendering of map. Since the initialMapBounds are null, the view of
-                the map will default to the longitude, latitude, and zoom defined in the initialStateView prop of the Map component, so
-                0, 0, and 1, respectively. */
-                setViewLoaded(true)
-            }
-        },
-        [observations, startingViewLoaded, setViewLoaded, setInitialMapBounds, initialMapBounds]
-    )
-  
+
+    const initialMapBounds = useMemo(() => {
+      if (observations.length >= 1) {
+        let mapBounds = new mapboxgl.LngLatBounds()
+        observations.forEach((obs) => {
+          mapBounds.extend([obs.longitude, obs.latitude])
+        })
+        console.log(mapBounds)
+        return mapBounds
+        // If there are no observations yet, set initialMapBounds to null 
+      } else if (observations.length < 1) {
+        /* Since the initialMapBounds are null, the view of the map will default to the longitude,
+        latitude, and zoom defined in the initialStateView prop of the Map component, so
+        0, 0, and 1, respectively. */
+        return null
+      }
+    } ,[observations])
+    
+    //CREATE MARKER ON MAP FOR EACH OBSERVATION
     const markers = useMemo(() => observations.map(obj => (
       <Marker key={obj.id}
         longitude={obj.longitude}
@@ -76,7 +71,6 @@ import Map, {
     const API_KEY = import.meta.env.VITE_MAPBOX_TOKEN
   
     return (
-    startingViewLoaded &&
       <section className="studyMapContainer">
         <Map
         mapboxAccessToken={API_KEY}
