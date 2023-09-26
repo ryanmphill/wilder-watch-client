68f0026df50e94005a1b7964d56f4c56e63a9735
Ryan Phillips <ryanmphill@gmail.com>
2023-09-25 20:02:11 -0500

    Changed method for handling initial map view by using extend method on lnglatbounds object. Updated comments for process

--- src/components/studies/studyDetails/StudyMap.jsx
+++ src/components/studies/studyDetails/StudyMap.jsx
@@ -13,7 +13,7 @@ import Map, {
 import mapboxgl from 'mapbox-gl';
   
   
-  const StudyMap = ({ observations, centerLon, centerLat, furthestLon, furthestLat }) => {
+  const StudyMap = ({ observations, centerLat }) => {
   
     const [popupDisplay, setPopupDisplay] = useState(null)
     const [currentGeolocation, setCurrentGeolocation] = useState(null)
@@ -22,32 +22,33 @@ import mapboxgl from 'mapbox-gl';
     const [startingViewLoaded, setViewLoaded] = useState(false)
 
     // MAKE ALL MARKERS VISIBLE ON INITIAL RENDERING OF MAP ///////////////////////////////////////////////////////////
-    /* This useEffect takes the center observation point and the point furthest from it (both calculated by the server) 
-        to set the initial map view. The mapbox gl library lets us create instances of the LngLat class and use those
-        instances to get the radius in meters and then create a `LngLatBounds` object that the Map can use to set the initial view
-        that will allow all of the observation markers to be visible. */
+    /* This useEffect creates an instance of `LngLatBounds` from the MapboxGl library that the Map can use to 
+       set the initial view and allow all of the observation markers to be visible. The LngLatBounds object 
+       characterizes an imaginary bounding box with two properties: a coordinate for the northeast corner, and 
+       a coordinate for the southwest corner. First, an object with null values is initialized. Next, the .extend()
+       method incrementally expands the bounding box to include each coordinates for the markers in the `observations`
+       array. As this happens, the NE and SW properties are automatically calculated and normalized to account for
+       point near the edge of the map or across the antimeridian.  */
     useEffect(
         () => {
-            if (typeof centerLon === 'number' && typeof centerLat === 'number' && furthestLon && furthestLat) {
+            console.log("obs length", observations.length)
+            console.log("starting view loaded", startingViewLoaded)
+            if (observations.length >= 1 && !startingViewLoaded) {
                 let mapBounds = new mapboxgl.LngLatBounds()
                 observations.forEach((obs) => {
                   mapBounds.extend([obs.longitude, obs.latitude])
                 })
-                // const center = new mapboxgl.LngLat(centerLon, centerLat)
-                // const furthest = new mapboxgl.LngLat(furthestLon, furthestLat)
-                // const radius = center.distanceTo(furthest)
-                // const mapBounds = center.toBounds(radius)
                 console.log(mapBounds)
                 setInitialMapBounds(mapBounds)
-            // If there are no observations yet, the center points default to zero, and the furthest points will be null 
-            } else if (typeof centerLon === 'number' && typeof centerLat === 'number' && furthestLon === null && furthestLat === null) {
+            // If there are no observations yet, set startingViewLoaded to true and leave initialMapBounds null 
+            } else if (observations.length < 1 && !startingViewLoaded && centerLat === 0) {
                 /* Set startingViewLoaded to true to allow rendering of map. Since the initialMapBounds are null, the view of
                 the map will default to the longitude, latitude, and zoom defined in the initialStateView prop of the Map component, so
                 0, 0, and 1, respectively. */
                 setViewLoaded(true)
             }
         },
-        [centerLon, centerLat, furthestLon, furthestLat]
+        [observations, startingViewLoaded, setViewLoaded, setInitialMapBounds, centerLat]
     )
 
     // If and when the initial map bounds are set, set startingViewLoaded to true and allow rendering of map
@@ -93,8 +94,8 @@ import mapboxgl from 'mapbox-gl';
         initialViewState={{
           bounds: initialMapBounds,
           fitBoundsOptions: {padding: 50},
-          longitude: centerLon,
-          latitude: centerLat,
+          longitude: 0,
+          latitude: 0,
           zoom: 1
         }}
         style={{width: '100%', height: '75vh'}}
